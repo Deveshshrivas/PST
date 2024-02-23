dbe9619e7fddd507caaf8e08edb0743edfb28b24
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2024-02-23 12:19:27 +0000

    committed

---
+++ pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int m =n;
+    // cout<<n;
+    for(int i=0; i<n; i++){ 
+        for(int j = 0; j<n; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+
+    }
+
+    return 0;
+}
