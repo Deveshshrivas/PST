6a99d4f0219a24b0ff7bfdca0236e62010070db6
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2024-02-24 11:00:37 +0000

    pattern17

--- pattern17.cpp
+++ pattern17.cpp
@@ -5,7 +5,7 @@ int main(){
     cin>>n;
     for(int i=0; i<n; i++){
        for(int j = 0; j < n - i - 1; j++) {
-            cout << "  ";
+            // cout << "  ";
         }
         for(int j = 0; j <= i; j++) {
             cout << char('A' + j) << " ";
